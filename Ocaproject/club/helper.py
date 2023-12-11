96f06e76fd0d2196ed7fc1145e831b8e7f5a8139
Nirnoy-Chandra-Sarker <nirnoy.chandra.sarker@g.bracu.ac.bd>
2023-12-11 23:05:43 +0600

    neew Commit

--- Ocaproject/club/helper.py
+++
@@ -1,6 +0,0 @@
-def get_designation(Student, Club, Model):
-    membership = Model.objects.get(student = Student, club = Club)
-    return membership.designation
-
-def calculate_remaining(a, b):
-    return a - b
